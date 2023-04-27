7df12e651d6469c45d089faf64e50fffb88fa6ec
Irina <iparakhina91@gmail.com>
2023-04-27 21:06:41 +0300

    changed test

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -22,6 +22,15 @@ public class TestBase {
         Configuration.browserVersion = "112.0";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+
+        Configuration.browserCapabilities = capabilities;
     }
 
     @BeforeEach
