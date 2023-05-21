64663d22c0ebc39de5616d90bf11e77a839291d0
Irina <iparakhina91@gmail.com>
2023-05-21 13:40:05 +0300

    added properties

---
+++ src/test/java/tests/OpenFormTest.java
@@ -0,0 +1,17 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
+
+public class OpenFormTest {
+
+    @Test
+    public void openFormTest() {
+        step("Open form ", () -> {
+        });
+
+        step("Close banners", () -> {
+        });
+    }
+}
